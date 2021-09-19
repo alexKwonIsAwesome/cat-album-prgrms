45ff091a3db8bae39beca2a01a4324b426a4bcc6
Alex Kwon <khwalex@gmail.com>
2021-09-19 20:11:40 +0900

    Add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,16 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react"],
-  rules: {},
+  plugins: ['react'],
+  rules: {
+    'no-unused-vars': 'off',
+  },
 };
